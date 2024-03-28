14219e959e7ad60e95f12f7eaca8f47f50f5eba2
tmuzikova <tereza.muzikova@gmail.com>
2024-03-28 10:53:54 +0100

    added animation to generating poem line

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ function generatePoem(event) {
   let userInstruction = document.getElementById("user-instructions");
   let apiKey = "6ddtf02cf3a61b83b8oaf010eba5f493";
   let context =
-    "you are a funny poem writer and love to write short poems with funny jokey elements. Please provide just the poem in an HTML format, the poem shuld have just 4 lines, each of them devided by <br />, do not write anything else, just the poem itself";
+    "you are a funny poem writer and love to write short poems with funny jokey elements. Please provide just the poem in an HTML format, the poem shuld have just 4 lines, each of them devided by <br />, do not write anything else, just the poem itself, do not include a title";
   let prompt = `generate a poem in english about ${userInstruction.value}, please`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `Generating poem about ${userInstruction.value}`;
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${userInstruction.value} </div>`;
   axios.get(apiURL).then(displayPoem); //call API
 }
 
